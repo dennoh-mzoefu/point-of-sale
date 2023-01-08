5716503ef391ad7be75f166c5627c60bfa2748fc
Denis Gichana <denisgichana162@gmail.com>
2023-01-08 23:26:16 +0300

    WIP

---
+++ src/assets/dummy.js
@@ -0,0 +1,35 @@
+import mandazi from "./mandazi.jpg";
+import chapati from "./chapati.jpg";
+import nduma from "./nduma.jpg";
+import ngwaci from "./ngwaci.jpg";
+
+export const food = [
+  {
+    id: 1,
+    category: "snack",
+    foodName: "chapati",
+    image: chapati,
+    price: 20,
+  },
+  {
+    id: 2,
+    category: "snack",
+    foodName: "mandazi",
+    image: mandazi,
+    price: 30,
+  },
+  {
+    id: 3,
+    category: "snack",
+    foodName: "nduma",
+    image: nduma,
+    price: 43,
+  },
+  {
+    id: 4,
+    category: "snack",
+    foodName: "ngwaci",
+    image: ngwaci,
+    price: 26,
+  },
+];
