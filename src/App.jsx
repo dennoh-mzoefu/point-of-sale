316adae4c02c2ef28c0acfb66d6ca414cb6509ed
Denis Gichana <denisgichana162@gmail.com>
2023-01-16 23:14:06 +0300

    add auth

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import { useState } from "react";
 import "./App.css";
 import Menu from "./components/Menu/Menu";
 import Checkout from "./components/Checkout/Checkout";
+import Sidebar from "./components/Sidebar";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div className="App">
-      <Menu />
+      <h1 className="text-cyan-900">Hello world</h1>
+      {/* <Sidebar /> */}
+      <Login />
+      {/* <Menu /> */}
       {/* <Checkout /> */}
     </div>
   );
