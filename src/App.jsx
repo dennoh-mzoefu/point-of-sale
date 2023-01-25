5e764a78280b326bf670079fac6e2dcf2a9cc855
Denis Gichana <denisgichana162@gmail.com>
2023-01-25 23:40:40 +0300

    route pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,36 @@ import "./App.css";
 import Menu from "./components/Menu/Menu";
 import Checkout from "./components/Checkout/Checkout";
 import Sidebar from "./components/Sidebar";
-// import Login from "./pages/Login";
+import Login from "./pages/Login";
 import Admin from "./pages/Admin";
 import { useSelector } from "react-redux";
 import PreApp from "./PreApp";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
+import Sales from "./pages/Sales";
+import Employees from "./pages/Employees";
+import Expenses from "./pages/Expenses";
+import Store from "./pages/Store";
 
 function App() {
   return (
     <div className="flex flex-col bg-slate-200">
       <Navbar />
       <PreApp className="w-0 h-0" />
-
       <div className="flex">
         <Sidebar />
-        <Home className="" />
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/sales" element={<Sales />} />
+          <Route path="/employees" element={<Employees />} />
+          <Route path="/expenses" element={<Expenses />} />
+          <Route path="/store" element={<Store />} />
+        </Routes>
       </div>
+
       {/* <Login /> */}
       {/* <Admin /> */}
       {/* <Menu /> */}
