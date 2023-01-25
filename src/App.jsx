46002cef3a8cbb9caf058f0ae74ab680cb459249
Denis Gichana <denisgichana162@gmail.com>
2023-01-25 21:03:24 +0300

    render components

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,25 @@ import "./App.css";
 import Menu from "./components/Menu/Menu";
 import Checkout from "./components/Checkout/Checkout";
 import Sidebar from "./components/Sidebar";
-import Login from "./pages/Login";
+// import Login from "./pages/Login";
+import Admin from "./pages/Admin";
+import { useSelector } from "react-redux";
+import PreApp from "./PreApp";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-cyan-900">Hello world</h1>
-      {/* <Sidebar /> */}
-      <Login />
+    <div className="flex flex-col bg-slate-200">
+      <Navbar />
+      <PreApp className="w-0 h-0" />
+
+      <div className="flex">
+        <Sidebar />
+        <Home className="" />
+      </div>
+      {/* <Login /> */}
+      {/* <Admin /> */}
       {/* <Menu /> */}
       {/* <Checkout /> */}
     </div>
