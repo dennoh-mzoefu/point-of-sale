f04b5bc0f813662be6dc3eed7383e0856b3f878d
Denis Gichana <denisgichana162@gmail.com>
2023-01-25 23:38:03 +0300

    styled

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { AiOutlineCloseCircle } from "react-icons/ai";
 import { AiOutlineHome } from "react-icons/ai";
+import { MdPeople } from "react-icons/md";
+import { FcSalesPerformance, FcMoneyTransfer } from "react-icons/fc";
+// import { FcSalesPerformance } from "react-icons/fc";
 
 function Sidebar() {
   return (
@@ -9,30 +12,40 @@ function Sidebar() {
         <AiOutlineCloseCircle className="my-5 justify-end " />
         <div className="flex flex-col">
           {/* each link */}
-          <div className="w-52 flex  items-center justify-center  py-3 hover:text-white  hover:bg-blue-400">
-            {/* icon  */}
-            <AiOutlineHome className="text-xl " />
-            <h3 className="ml-4 font-semibold">Home</h3>
+          <div className="w-52 flex  items-center justify-center py-3 hover:text-white  hover:bg-blue-400">
+            <div className="w-3/4 flex  items-center justify-items-start   ">
+              {/* icon  */}
+              <AiOutlineHome className="text-xl " />
+              <h3 className="ml-4 font-semibold">Home</h3>
+            </div>
           </div>
-          <div className="w-52 flex  items-center justify-center  py-3 hover:text-white  hover:bg-blue-400">
-            {/* icon  */}
-            <AiOutlineHome className="text-xl " />
-            <h3 className="ml-4 font-semibold">Stock</h3>
+          <div className="w-52 flex  items-center justify-center py-3 hover:text-white  hover:bg-blue-400">
+            <div className="w-3/4 flex  items-center justify-items-start   ">
+              {/* icon  */}
+              <MdPeople className="text-xl " />
+              <h3 className="ml-4 font-semibold">Employees</h3>
+            </div>
           </div>
-          <div className="w-52 flex  items-center justify-center  py-3 hover:text-white  hover:bg-blue-400">
-            {/* icon  */}
-            <AiOutlineHome className="text-xl " />
-            <h3 className="ml-4 font-semibold ">Orders</h3>
+          <div className="w-52 flex  items-center justify-center py-3 hover:text-white  hover:bg-blue-400">
+            <div className="w-3/4 flex  items-center justify-items-start   ">
+              {/* icon  */}
+              <AiOutlineHome className="text-xl " />
+              <h3 className="ml-4 font-semibold ">Orders</h3>
+            </div>
           </div>
-          <div className="w-52 flex  items-center justify-center  py-3 hover:text-white hover:bg-blue-400">
-            {/* icon  */}
-            <AiOutlineHome className="text-xl " />
-            <h3 className="ml-4 font-semibold">Sales</h3>
+          <div className="w-52 flex  items-center justify-center py-3 hover:text-white  hover:bg-blue-400">
+            <div className="w-3/4 flex  items-center justify-items-start   ">
+              {/* icon  */}
+              <FcSalesPerformance className="text-xl " />
+              <h3 className="ml-4 font-semibold">Sales</h3>
+            </div>
           </div>
-          <div className="w-52 flex  items-center justify-center  py-3 hover:text-white  hover:bg-blue-400">
-            {/* icon  */}
-            <AiOutlineHome className="text-xl " />
-            <h3 className="ml-4 font-semibold">Expenses</h3>
+          <div className="w-52 flex  items-center justify-center py-3 hover:text-white  hover:bg-blue-400">
+            <div className="w-3/4 flex  items-center justify-items-start   ">
+              {/* icon  */}
+              <FcMoneyTransfer className="text-xl " />
+              <h3 className="ml-4 font-semibold">Expenses</h3>
+            </div>
           </div>
         </div>
       </div>
