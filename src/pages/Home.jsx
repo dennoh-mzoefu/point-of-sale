ffda5394d87fe77a9feaabca5ab265d4224bc3e5
Denis Gichana <denisgichana162@gmail.com>
2023-01-25 20:59:50 +0300

    create and style

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="bg-white shadow-inner rounded-tl-md w-full mt-2 ml-2 border-l border-t border-slate-300/95">
+      Home
+    </div>
+  );
+}
+
+export default Home;
