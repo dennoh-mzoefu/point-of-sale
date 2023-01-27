6c057c935035112321aeb4b1181cd707552d9fa8
Denis Gichana <denisgichana162@gmail.com>
2023-01-27 17:24:56 +0300

    create page

---
+++ src/pages/MenuManager.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function MenuManager() {
+  // add , update, delete,  menu items
+  // see if menu item is out of stock
+  return (
+    <div>
+      {/* table that displays menu details in editable inputs and an update button, and a delete button  */}
+
+      <AddStock className="w-full" />
+    </div>
+  );
+}
+
+export default MenuManager;
