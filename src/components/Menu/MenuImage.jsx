f0e1fa68c8ac7f9854850507baefd827cee6db6e
Denis Gichana <denisgichana162@gmail.com>
2023-01-27 17:21:48 +0300

    get image thumbnail from firebase

---
+++ src/components/Menu/MenuImage.jsx
@@ -0,0 +1,22 @@
+import { getDownloadURL, ref } from "firebase/storage";
+import React, { useEffect, useState } from "react";
+import { storage } from "../../../utils/firebase";
+
+function MenuImage({ imageRef }) {
+  const [url, setUrl] = useState("");
+  useEffect(() => {
+    imageRef &&
+      getDownloadURL(ref(storage, imageRef))
+        .then((response) => {
+          console.log(response);
+          setUrl(response);
+        })
+        .catch((error) => {
+          console.log(error);
+          return;
+        });
+  }, []);
+  return <img className="h-44 w-48 rounded-t-lg" src={url} alt="food pic" />;
+}
+
+export default MenuImage;
