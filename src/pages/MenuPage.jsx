9819946b51dcf23c66bf38e0ff1402f39277e7c0
Denis Gichana <denisgichana162@gmail.com>
2023-01-27 17:25:05 +0300

    create page

--- src/pages/MenuPage.jsx
+++ src/pages/MenuPage.jsx
@@ -4,7 +4,7 @@ import Menu from "../components/Menu/Menu";
 
 function MenuPage() {
   return (
-    <div className="bg-white shadow-inner rounded-tl-md w-full mt-2 ml-2 border-l border-t border-slate-300/95">
+    <div className=" w-full mt-2 ml-2 ">
       <div className="flex w-full">
         <Menu className="" />
         <AddStock className="w-full" />
