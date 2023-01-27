ce14bb4126b1d33a1584ee97194c5abc217dbd52
Denis Gichana <denisgichana162@gmail.com>
2023-01-27 17:26:13 +0300

    add order slice to  store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import menuReducer from "./menuSlice";
+import orderReducer from "./orderSlice";
 
 export const store = configureStore({
   reducer: {
     menu: menuReducer,
+    order: orderReducer,
   },
 });
