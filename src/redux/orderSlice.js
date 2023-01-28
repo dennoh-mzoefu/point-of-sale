b6b8e676ae8ded95686fec112d230964c7ef0b24
Denis Gichana <denisgichana162@gmail.com>
2023-01-28 23:24:19 +0300

    currntOrders state

--- src/redux/orderSlice.js
+++ src/redux/orderSlice.js
@@ -2,6 +2,7 @@ import { createSlice, current } from "@reduxjs/toolkit";
 import { addDoc } from "firebase/firestore";
 import { ordersColRef } from "../../utils/firebase";
 const initialState = {
+  currentOrders: [],
   orders: [],
   order: {},
 };
@@ -14,7 +15,14 @@ export const orderSlice = createSlice({
       state.orders = action.payload;
       console.log(current(state));
     },
+    addCurrentOrder: (state, action) => {
+      let a = state.currentOrders;
+      state.currentOrders.push(action.payload);
+      // console.log(current(state));
+    },
     addOrder: (state, action) => {
+      var index = state.currentOrders.indexOf(action.payload);
+      state.currentOrders.splice(index, 1);
       console.log(action.payload);
       addDoc(ordersColRef, {
         quantity: action.payload.quantity,
@@ -28,5 +36,5 @@ export const orderSlice = createSlice({
   },
 });
 
-export const { fetchOrder } = orderSlice.actions;
+export const { fetchOrder, addCurrentOrder, addOrder } = orderSlice.actions;
 export default orderSlice.reducer;
