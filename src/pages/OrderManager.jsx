ef0cce9b6c7068e1ce1ee6c1082ac34dfbbc3041
Denis Gichana <denisgichana162@gmail.com>
2023-01-28 23:23:54 +0300

    display orders

---
+++ src/pages/OrderManager.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AddStock from "../components/AddStock";
+import Orders from "../components/Menu/Orders";
+
+function OrderManager() {
+  return (
+    <div>
+     <Orders/>
+    </div>
+  );
+}
+
+export default OrderManager;
