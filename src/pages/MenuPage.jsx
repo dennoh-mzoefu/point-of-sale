2544a51db33fdb0535529d8c09f9a6042de5b0fc
Denis Gichana <denisgichana162@gmail.com>
2023-01-29 22:35:20 +0300

    WIP

--- src/pages/MenuPage.jsx
+++ src/pages/MenuPage.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import AddStock from "../components/AddStock";
 import Menu from "../components/Menu/Menu";
+import PreOrders from "../components/Orders/PreOrders";
 
 function MenuPage() {
   return (
     <div className=" w-full mt-2 ml-2 ">
       <div className="flex w-full">
         <Menu className="" />
-        <AddStock className="w-full" />
+        <div className="w-full flex flex-col">
+          <PreOrders />
+          <AddStock />
+        </div>
       </div>
     </div>
   );
