df9c2520b722597e05871b73e9d1810c4e40118c
Denis Gichana <denisgichana162@gmail.com>
2023-01-30 22:46:10 +0300

    styling

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import { useSelector } from "react-redux";
 import OrderItem from "./OrderItem";
 
@@ -6,25 +6,29 @@ function Orders() {
   const { orders } = useSelector((state) => state.order);
 
   return (
-    <div>
-      {orders?.map((item) => {
-        return (
-          <div>
-            {console.log(item, "orders")}
-            <table>
-              <tr>
-                <th>Name</th>
-                <th>Quantity</th>
-                <th>Price</th>
-                <th>Done</th>
-                <th>Cancelled</th>
-                <th>Delete</th>
-              </tr>
-              <OrderItem order={item} />
-            </table>
-          </div>
-        );
-      })}
+    <div className="flex flex-col justify-center items-center   mt-5">
+      <p className="text-xl bg-white p-3 w-3/4 text-center">Orders Manager</p>
+      <hr className="my-2"></hr>
+      <table className=" w-3/4 mx-auto  text-left text-gray-500">
+        <thead className="text-md text-gray-700 uppercase bg-blue-200">
+          <tr>
+            <th className="px-6 py-3 border border-white">Meal</th>
+            <th className="px-6 py-3 border border-white">Quantity</th>
+            <th className="px-6 py-3 border border-white">Price</th>
+            <th className="px-6 py-3 border border-white">Done</th>
+            <th className="px-6 py-3 border border-white">Cancelled</th>
+            <th className="px-6 py-3 border border-white">Delete</th>
+          </tr>
+        </thead>
+        {orders?.map((item, index) => {
+          return (
+            <Fragment key={index}>
+              {console.log(item, "orders")}
+              <OrderItem order={item} index={index} />
+            </Fragment>
+          );
+        })}
+      </table>
     </div>
   );
 }
