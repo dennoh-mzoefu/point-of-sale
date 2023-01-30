affd77f9e2d262d4af23a3ab20bdc9ec8bc006a9
Denis Gichana <denisgichana162@gmail.com>
2023-01-30 22:45:58 +0300

    styling deleting doc

--- src/components/Orders/OrderItem.jsx
+++ src/components/Orders/OrderItem.jsx
@@ -1,24 +1,40 @@
 import React, { useState } from "react";
-
-function OrderItem({ order }) {
+import { AiFillDelete } from "react-icons/ai";
+import { useDispatch } from "react-redux";
+import { deleteOrder } from "../../redux/orderSlice";
+function OrderItem({ order, index }) {
   const [ordermanaged, setorderManaged] = useState(order);
+  console.log(ordermanaged);
+  const dispatch = useDispatch();
+  con;
+  const handleDelete = (order) => {
+    dispatch(deleteOrder(order.time));
+  };
   return (
-    <div>
-      <tr>
-        <td>{ordermanaged.name}</td>
-        <td>2</td>
-        <td>{ordermanaged.price}</td>
-        <td>
+    <tbody>
+      <tr
+        className={
+          index % 2 == 1
+            ? "bg-slate-300 text-black p-1 text-xl"
+            : "bg-slate-800 text-white p-1 text-xl"
+        }
+      >
+        <td className="p-1">{ordermanaged.name}</td>
+        <td className="p-1">2</td>
+        <td className="p-1">{ordermanaged.price}</td>
+        <td className="p-1">
           <input type="checkbox" />
         </td>
-        <td>
+        <td className="p-1">
           <input type="checkbox" />
         </td>
-        <td>
-          <button>Delete</button>
+        <td className="p-1 ">
+          <button className="">
+            <AiFillDelete onClick={() => handleDelete()} />
+          </button>
         </td>
       </tr>
-    </div>
+    </tbody>
   );
 }
 
