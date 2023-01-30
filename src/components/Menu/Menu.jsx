945534aae1f93377974d36d2026447e83e918e50
Denis Gichana <denisgichana162@gmail.com>
2023-01-30 22:45:20 +0300

    styling

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { BsDot } from "react-icons/bs";
+import { v4 } from "uuid";
 import { addCurrentOrder } from "../../redux/orderSlice";
 import MenuImage from "./MenuImage";
 
@@ -17,6 +19,7 @@ function Menu() {
         quantity: 1,
         isPrepared: false,
         isCancelled: false,
+        orderId: v4(),
       })
     );
   };
@@ -31,19 +34,33 @@ function Menu() {
         {menu?.map((item, index) => {
           return (
             <div
-              className="flex flex-col bg-white shadow-lg m-2 w-fit rounded-lg"
+              className="flex  justify-center  flex-col bg-blue-100 shadow-lg m-2 w-fit rounded-lg"
               key={index}
             >
               <div className="flex flex-col">
                 {/* <div className="w-0 h-0" onLoad={console.log({ url })}></div> */}
                 <MenuImage imageRef={item.imageRef} />
                 {/* name */}
-                <p className="py-1 pl-10 pr-5">{item.name}</p>
-                <p className="py-1 pl-10 pr-5">{item.category}</p>
+                <p className="pt-1  flex pl-10 pr-5">
+                  <BsDot />
+                  {item.name}
+                </p>
+                <p className="pt-1 flex pl-10 pr-5">
+                  <BsDot />
+                  {item.category}
+                </p>
                 {/* price */}
-                <p className="py-1 pl-10 pr-5">Ksh {item.price}</p>
+                <p className="pt-1 flex pl-10 pr-5">
+                  <BsDot />
+                  Ksh {item.price}
+                </p>
               </div>
-              <button onClick={() => handleOrder(item)}>Place Order</button>
+              <button
+                className="bg-slate-400 text-white w-fit rounded-md p-1 mx-auto mb-1"
+                onClick={() => handleOrder(item)}
+              >
+                Place Order
+              </button>
             </div>
           );
         })}
