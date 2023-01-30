c2994615b6a41528126e563c42f30b7518cd3d9f
Denis Gichana <denisgichana162@gmail.com>
2023-01-30 22:46:53 +0300

    styling

--- src/components/Orders/PreOrders.jsx
+++ src/components/Orders/PreOrders.jsx
@@ -5,8 +5,9 @@ import PreOrderItem from "./PreOrderItem";
 function PreOrders() {
   const { currentOrders } = useSelector((state) => state.order);
   return (
-    <div className="">
-      Order List
+    <div className="rounded-md ml-4 w-fit bg-white p-2">
+      <p className="text-center text-xl -black h-2xl mb-2">Order List</p>
+      <hr className="mb-3"></hr>
       {currentOrders?.map((item, index) => {
         return (
           <div key={index}>
