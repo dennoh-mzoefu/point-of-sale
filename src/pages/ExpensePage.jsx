b1f7fcfe97c272e20a0599a0a41123d04ad4427c
Denis Gichana <denisgichana162@gmail.com>
2023-02-01 23:58:54 +0300

    expenses

---
+++ src/pages/ExpensePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Expense from "../components/Expense";
+
+function ExpensePage() {
+  return (
+    <div className=" w-full">
+      <Expense />
+    </div>
+  );
+}
+
+export default ExpensePage;
