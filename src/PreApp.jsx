ae52fe16dc1f370a068045ee58134f33faa10b98
Denis Gichana <denisgichana162@gmail.com>
2023-02-02 00:00:32 +0300

    add snapshot

--- src/PreApp.jsx
+++ src/PreApp.jsx
@@ -1,31 +1,44 @@
 import { onSnapshot } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
-import { colRef, ordersColRef } from "../utils/firebase";
+import { colRef, expenseColRef, ordersColRef } from "../utils/firebase";
+import { fetchExpenses } from "./redux/expenseSlice";
 import { fetchMenu } from "./redux/menuSlice";
 import { fetchOrders } from "./redux/orderSlice";
 
 function PreApp() {
   const [menuItems, setMenuItems] = useState([]);
   const [orders, setOrders] = useState([]);
+  const [expense, setExpense] = useState([]);
   const dispatch = useDispatch();
   let b = [];
   useEffect(() => {
-    onSnapshot(ordersColRef, (snapshot) => {
-      setOrders(snapshot.docs.map((doc) => doc.data()));
+    const unSubscribe = onSnapshot(expenseColRef, (snapshot) => {
+      setExpense(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
     });
+    return () => unSubscribe();
   }, []);
   useEffect(() => {
-    onSnapshot(colRef, (snapshot) => {
+    const unSubscribe = onSnapshot(ordersColRef, (snapshot) => {
+      setOrders(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    });
+    return () => unSubscribe();
+  }, []);
+  useEffect(() => {
+    const unSubscribe = onSnapshot(colRef, (snapshot) => {
       setMenuItems(snapshot.docs.map((doc) => doc.data()));
     });
+    return () => unSubscribe();
   }, []);
   useEffect(() => {
     menuItems.length !== 0 && dispatch(fetchMenu(menuItems));
   }, [menuItems]);
   useEffect(() => {
-    orders.length !== 0 && dispatch(fetchOrders(orders));
-  }, [menuItems]);
+    dispatch(fetchOrders(orders));
+  }, [orders]);
+  useEffect(() => {
+    dispatch(fetchExpenses(expense));
+  }, [expense]);
   console.log({ menuItems });
   console.log({ orders });
 
