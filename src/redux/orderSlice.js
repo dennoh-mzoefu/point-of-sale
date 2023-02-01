45cac558fbb662b78718b278eeb2a2133ef46cea
Denis Gichana <denisgichana162@gmail.com>
2023-02-01 23:59:42 +0300

    finish reducer

--- src/redux/orderSlice.js
+++ src/redux/orderSlice.js
@@ -1,28 +1,36 @@
 import { createSlice, current } from "@reduxjs/toolkit";
-import { addDoc, deleteDoc } from "firebase/firestore";
+import { addDoc, deleteDoc, doc } from "firebase/firestore";
 import { db, ordersColRef } from "../../utils/firebase";
 const initialState = {
   currentOrders: [],
   orders: [],
   order: {},
+  change: false,
 };
 
 export const orderSlice = createSlice({
   name: "orders",
   initialState,
   reducers: {
+    changeListener: (state, action) => {
+      // state.change =
+    },
     fetchOrders: (state, action) => {
-      state.orders = action.payload;
       console.log(current(state));
+      return {
+        ...state,
+        orders: action.payload,
+      };
     },
     addCurrentOrder: (state, action) => {
       let a = state.currentOrders;
+      console.log(action.payload);
       state.currentOrders.push(action.payload);
       // console.log(current(state));
     },
     addOrder: (state, action) => {
-      var index = state.currentOrders.indexOf(action.payload);
-      state.currentOrders.splice(index, 1);
+      // var index = state.currentOrders.indexOf(action.payload);
+      // state.currentOrders.splice(index, 1);
       console.log(action.payload);
       addDoc(ordersColRef, {
         quantity: action.payload.quantity,
@@ -30,23 +38,32 @@ export const orderSlice = createSlice({
         price: action.payload.price,
         isPrepared: action.payload.isPrepared,
         isCancelled: action.payload.isCancelled,
-        time: Date.now(),
+        time: new Date(),
       }).then((res) => {
         console.log(res);
       });
+      return {
+        ...state,
+        currentOrders: state.currentOrders.filter(
+          (item) => item.orderId !== action.payload.orderId
+        ),
+      };
     },
     deleteOrder: (state, action) => {
       const docRef = doc(db, "orders", action.payload);
-
+      console.log("delete");
       deleteDoc(docRef).then(() => {
         console.log(action.payload);
+        // const docRef = doc(db, 'orders',action.payload)
       });
     },
     deleteCurrentOrder: (state, action) => {
-      console.log(current(state));
-      console.log(action.payload);
-      var index = state.currentOrders.indexOf(action.payload);
-      state.currentOrders.splice(index, 1);
+      return {
+        ...state,
+        currentOrders: state.currentOrders.filter(
+          (item) => item.orderId !== action.payload
+        ),
+      };
     },
   },
 });
