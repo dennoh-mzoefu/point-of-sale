7d9e436a00e0a40f22f8aad8e8eb125d3f93b258
Denis Gichana <denisgichana162@gmail.com>
2023-02-01 23:59:55 +0300

    updtae store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,16 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import menuReducer from "./menuSlice";
 import orderReducer from "./orderSlice";
+import expenseReducer from "./expenseSlice";
 
 export const store = configureStore({
   reducer: {
     menu: menuReducer,
     order: orderReducer,
+    expense: expenseReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
