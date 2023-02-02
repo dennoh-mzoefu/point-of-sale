7ead9d13ef1f6a5885760db280f043e64232d4ed
Denis Gichana <denisgichana162@gmail.com>
2023-02-02 22:41:22 +0300

    styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,13 +5,14 @@ import logo from "../assets/resturant-logo.svg";
 
 function Navbar() {
   return (
-    <div className="flex justify-between p-4 items-center bg-blue-500">
+    <div className="flex justify-between w-full p-4 items-center bg-green-500">
       <div className="flex items-center">
         <img src={logo} className="h-10" />
         <h1 className="ml-4 text 2xl">Green Pastures Hotel</h1>
       </div>
       <div className="flex">
         <BiUserCircle className="text-3xl  mx-5" />
+        <p className="text-xl">Logout</p>
         <GrNotification className="text-3xl mx-5" />
       </div>
     </div>
