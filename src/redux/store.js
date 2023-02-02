01c5cf08566ece07004fddd3f789e1b19eb3df91
Denis Gichana <denisgichana162@gmail.com>
2023-02-02 22:40:09 +0300

    stock reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,12 +3,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import menuReducer from "./menuSlice";
 import orderReducer from "./orderSlice";
 import expenseReducer from "./expenseSlice";
+import stockReducer from "./stockSlice";
 
 export const store = configureStore({
   reducer: {
     menu: menuReducer,
     order: orderReducer,
     expense: expenseReducer,
+    stock: stockReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
