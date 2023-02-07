b7c30c955713852bf444969772de75c1245f8447
Denis Gichana <denisgichana162@gmail.com>
2023-02-07 23:59:20 +0300

    add sales card dashboard

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,18 +4,33 @@ import { MdOutlinePayment } from "react-icons/md";
 
 function Home() {
   const { expense } = useSelector((state) => state.expense);
-  const [today, setToday] = useState([]);
+  const { sales } = useSelector((state) => state.sales);
+  const [todayExpense, setTodayExpense] = useState([]);
+  const [todaySales, setTodaySales] = useState([]);
   const [totalExpense, setTotalExpense] = useState(0);
+  const [totalSales, setTotalSales] = useState(0);
   let t = new Date();
 
-  let newToday = new Date(t.toString().slice(0, 16));
+  let newTodayExpense = new Date(t.toString().slice(0, 16));
   useEffect(() => {
-    // today expenses
-    setToday(
+    // todayExpense expenses
+    setTodayExpense(
       expense.filter((item) => {
         let newDate = new Date(item.time.seconds * 1000);
         return (
-          newDate.getTime() >= newToday.getTime() &&
+          newDate.getTime() >= newTodayExpense.getTime() &&
+          newDate.getTime() < t.getTime()
+        );
+      })
+    );
+  }, [expense]);
+  useEffect(() => {
+    // todayExpense expenses
+    setTodaySales(
+      expense.filter((item) => {
+        let newDate = new Date(item.time.seconds * 1000);
+        return (
+          newDate.getTime() >= newTodayExpense.getTime() &&
           newDate.getTime() < t.getTime()
         );
       })
@@ -23,18 +38,32 @@ function Home() {
   }, [expense]);
   useEffect(() => {
     setTotalExpense(
-      today?.map((item) => parseInt(item.price)).reduce((a, b) => a + b, 0)
+      todayExpense
+        ?.map((item) => parseInt(item.price))
+        .reduce((a, b) => a + b, 0)
     );
-  }, [today]);
+  }, [todayExpense]);
+  useEffect(() => {
+    setTotalSales(
+      todaySales?.map((item) => parseInt(item.price)).reduce((a, b) => a + b, 0)
+    );
+  }, [todaySales]);
 
   return (
     <div className="flex justify-center p-6">
-      {console.log({ today })}
+      {console.log({ todayExpense })}
       {console.log({ totalExpense })}
       <div className="flex mx-4 py-8 px-4 w-64 bg-white justify-around">
         <div className="flex flex-col">
           <p>Total Expense</p>
           <p className="text-3xl">{totalExpense}</p>
+        </div>
+        <MdOutlinePayment className="text-2xl w-10 h-10 p-1 shadow-sm rounded-full bg-red-600" />
+      </div>
+      <div className="flex mx-4 py-8 px-4 w-64 bg-white justify-around">
+        <div className="flex flex-col">
+          <p>Total Sales</p>
+          <p className="text-3xl">{totalSales}</p>
         </div>
         <MdOutlinePayment className="text-2xl w-10 h-10 p-1 shadow-sm rounded-full bg-red-600" />
       </div>
