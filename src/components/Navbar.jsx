e2a17b2be15d2235bb914863b4874021e264b244
Denis Gichana <denisgichana162@gmail.com>
2023-02-08 22:24:11 +0300

    styling and auth

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,43 @@
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { BiUserCircle } from "react-icons/bi";
-import { GrNotification } from "react-icons/gr";
 import logo from "../assets/resturant-logo.svg";
+import { GrNotification } from "react-icons/gr";
+import { auth } from "../../utils/firebase";
 
 function Navbar() {
+  const [user, loading] = useAuthState(auth);
+  console.log({ user });
   return (
-    <div className="sticky top-0 z-10 flex justify-between w-full p-4 items-center bg-green-500">
-      <div className="flex items-center">
-        <img src={logo} className="h-10" />
-        <h1 className="ml-4 text 2xl">Green Pastures Hotel</h1>
-      </div>
+    <div className="overflow-hidden  sticky top-0 z-10 flex justify-between w-full p-4 items-center bg-zinc-50  border-b pb-2 border-b-green-700">
+      <div className="flex items-center"></div>
       <div className="flex">
-        <BiUserCircle className="text-3xl  mx-5" />
-        <p className="text-xl">Logout</p>
+        {user && (
+          <img
+            src={user?.photoURL}
+            // alt="pesonal image"
+            className="w-0"
+            width="50"
+            height="60"
+          />
+        )}
+
+        {user ? (
+          <img
+            src={user?.photoURL}
+            alt="pesonal image"
+            className="mx-5 rounded-full"
+            width="40"
+            height="40"
+          />
+        ) : (
+          <BiUserCircle className="text-3xl  mx-5" />
+        )}
+        {user ? (
+          <p className="text-xl">Logout</p>
+        ) : (
+          <p className="text-xl">Login</p>
+        )}
         <GrNotification className="text-3xl mx-5" />
       </div>
     </div>
