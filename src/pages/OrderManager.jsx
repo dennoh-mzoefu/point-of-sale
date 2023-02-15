2097c4d19cd2a96cf6611f31da3546905950b137
Denis Gichana <denisgichana162@gmail.com>
2023-02-15 23:24:57 +0300

    mobile responsive

--- src/pages/OrderManager.jsx
+++ src/pages/OrderManager.jsx
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AddStock from "../components/AddStock";
 import Orders from "../components/Orders/Orders";
 import Receipt from "../components/Receipt/Receipt";
+import { MdReceiptLong } from "react-icons/md";
+import { useSelector } from "react-redux";
 
 function OrderManager() {
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [screenSize, setScreenSize] = useState(0);
+
+  const { currentUser } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (screenSize <= 900) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(true);
+    }
+  }, [screenSize]);
   return (
     <div className="w-full flex ml-10 items-start justify-center">
       <Orders />
-      <Receipt />
+
+      {activeMenu ? (
+        <Receipt />
+      ) : (
+        <MdReceiptLong
+          className="p-1 mt-3 bg-white"
+          onClick={() => setActiveMenu(!activeMenu)}
+        />
+      )}
     </div>
   );
 }
