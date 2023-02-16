88b16c3070f6b02bd9a98052824b5a2da963ebb9
Denis Gichana <denisgichana162@gmail.com>
2023-02-16 17:51:04 +0300

    add printing functionality

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -6,18 +6,20 @@ function Orders() {
   const { orders } = useSelector((state) => state.order);
   console.log({ orders });
   return (
-    <div className="flex flex-col justify-center items-center w-6/7   mt-5">
-      <p className="text-xl bg-white p-3 w-3/4 text-center">Orders Manager</p>
+    <div className="flex flex-col justify-center items-center     mt-5">
+      <p className="text-xl bg-white p-3 w-3/4 max-w-fit text-center">
+        Orders Manager
+      </p>
       <hr className="my-2"></hr>
-      <table className=" w-3/4 mx-auto  text-left text-stone-500">
-        <thead className="text-md text-stone-700 uppercase bg-stone-200">
+      <table className=" w-3/4 max-w-fit mx-auto  text-left text-stone-500">
+        <thead className="w-fit text-md text-stone-700 uppercase bg-stone-200">
           <tr>
-            <th className="px-6 py-3 border border-white">Meal</th>
-            <th className="px-6 py-3 border border-white">Quantity</th>
-            <th className="px-6 py-3 border border-white">Price</th>
-            <th className="px-6 py-3 border border-white">Done</th>
-            <th className="px-6 py-3 border border-white">Cancelled</th>
-            <th className="px-6 py-3 border border-white">Delete</th>
+            <th className="px-2 py-3 border border-white">Meal</th>
+            <th className="px-2 py-3 border border-white">Quantity</th>
+            <th className="px-2 py-3 border border-white">Price</th>
+            <th className="px-2 py-3 border border-white">Done</th>
+            <th className="px-2 py-3 border border-white">Cancelled</th>
+            <th className="px-2 py-3 border border-white">Delete</th>
           </tr>
         </thead>
         {orders?.map((item, index) => {
